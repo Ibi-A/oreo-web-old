4a7357e6564930c95aac47eb5cfcb5a460fdfae5
Ibi Andrianoasy <ibi.andrianoasy@gmail.com>
2021-11-07 15:18:18 +0100

    created PictureModal component to display picture in fullscreen

---
+++ src/components/PictureModal.vue
@@ -0,0 +1,23 @@
+<template>
+  <b-modal :title="title" :id="'modal-' + postId + '-' + pictureId" size="xl" centered ok-only>
+    <b-img
+      :src="src"
+      class="mb-3"
+      fluid-grow
+    />
+    <p>{{ description }}</p>
+  </b-modal>
+</template>
+
+<script lang="ts">
+
+export default {
+    props: {
+        postId: Number,
+        pictureId: Number, 
+        title: String,
+        description: String,
+        src: String,
+    }
+}
+</script>
