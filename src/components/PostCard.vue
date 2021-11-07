a3e8dc4c1506f17fbbe687553e638cbbe3fc5cdc
Ibi Andrianoasy <ibi.andrianoasy@gmail.com>
2021-11-07 14:49:37 +0100

    created PostCard component

---
+++ src/components/PostCard.vue
@@ -0,0 +1,38 @@
+<template>
+  <b-card>
+    <b-carousel class="mb-3" controls indicators>
+      <b-carousel-slide
+        v-for="picture in pictures"
+        :key="picture.id"
+        :img-src="picture.src"
+      >
+        <h1>{{ picture.title }}</h1>
+        <p>{{ picture.description }}</p>
+      </b-carousel-slide>
+    </b-carousel>
+
+    <b-card-text>
+      <h1></h1>
+      <b-button v-b-toggle="'collapse-' + id" variant="link" class="mb-3"><h1>{{ title }}</h1></b-button>
+      <b-collapse :id="'collapse-' + id">
+        {{ description }}
+      </b-collapse>
+    </b-card-text>
+
+    <template #footer>
+      <b-badge>{{ date }}</b-badge>
+    </template>
+  </b-card>
+</template>
+
+<script lang="ts">
+export default {
+  props: {
+    id: Number,
+    date: String,
+    title: String,
+    description: String,
+    pictures: [],
+  },
+};
+</script>
