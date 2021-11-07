751733465c6e07087d520d7c1c1692beda018752
Ibi Andrianoasy <ibi.andrianoasy@gmail.com>
2021-11-07 14:50:13 +0100

    updated Home to use PostCard component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,30 @@
 <template>
   <b-container>
     <h1>Hello world!</h1>
+    <post-card
+      v-for="post in posts"
+      :key="post.id"
+      :id="post.id"
+      :date="post.date"
+      :title="post.title"
+      :description="post.description"
+      :pictures="post.pictures"  
+    />
   </b-container>
 </template>
 
 <script>
+import Posts from '../data/posts.json'
+import PostCard from '../components/PostCard.vue'
+
 export default {
-  components: {},
+  components: {
+    PostCard,
+  },
+  data() {
+    return {
+      posts: Posts
+    }
+  }
 };
 </script>
